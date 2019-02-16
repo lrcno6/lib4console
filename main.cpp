48ebde4c75dc375e36e42997d0848e0289dbd0ea
EnderPearl <1069442394@qq.com>
2019-02-16 18:53:26 +0800

    change it to command style

--- main.cpp
+++ main.cpp
@@ -1,120 +1,175 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<cstdio>
+#include<cstring>
 #include<cstdlib>
 #include<ctime>
 #include"libcore/library.h"
 using namespace lib;
 using namespace std;
+bool saved=true;
 string name;
 Library *library;
+void getline(string &str,bool loop=true){
+	auto read=[&str]{
+		getline(cin,str);
+		auto first=str.find_first_not_of(" \t\n\v\f\r"),last=str.find_last_not_of(" \t\n\v\f\r");
+		if(first!=string::npos)
+			str.erase(0,first);
+		if(last!=string::npos)
+			str.erase(last+1);
+	};
+	if(loop)
+		do
+			read();
+		while(str.empty());
+	else
+		read();
+}
+void version(){
+	cout<<"lib4console alpha 1.4.0\n"
+		<<"license: GPL-3.0\n"
+		<<"GitHub address: https://github.com/lrcno6/lib4console\n";
+}
 bool main_menu(){
-	char c;
-	cout<<"\n1.create a new ID\n"
-		<<"2.lend a book\n"
-		<<"3.return a book\n"
-		<<"4.sort by time for all the books\n"
-		<<"9.save and exit\n"
-		<<"0.exit without saving\n"
-		<<"You want to :";
-	mainloop:
-	cin>>c;
-	switch(c){
-		case '1':
-			cout<<Library::new_id(8)<<endl;
-			break;
-		case '2':{
-			string id,name;
-			cout<<"the book's ID:";
-			cin>>id;
-			cout<<"borrower:";
-			do
-				getline(cin,name);
-			while(name.empty());
-			try{
-				library->lend(id,name);
-			}
-			catch(logic_error&){
-				cerr<<"error:the ID is used\n";
-			}
-			break;
+	cout<<"> ";
+	string command;
+	getline(command,false);
+	if(command=="newid")
+		cout<<Library::new_id(8)<<endl;
+	else if(command=="lend"){
+		string id,name;
+		cout<<"the book's ID: ";
+		do
+			getline(id);
+		while(id.empty());
+		cout<<"borrower: ";
+		do
+			getline(name);
+		while(name.empty());
+		try{
+			library->lend(id,name);
+			saved=false;
 		}
-		case '3':{
-			string id;
-			cout<<"the book's ID:";
-			cin>>id;
-			try{
-				library->Return(id);
-			}
-			catch(out_of_range&){
-				cerr<<"error:the ID is not used\n";
-			}
-			break;
+		catch(logic_error&){
+			cerr<<"error: the ID is used\n";
 		}
-		case '4':
+	}
+	else if(command=="return"){
+		string id;
+		cout<<"the book's ID: ";
+		do
+			getline(id);
+		while(id.empty());
+		try{
+			library->Return(id);
+			saved=false;
+		}
+		catch(out_of_range&){
+			cerr<<"error: the ID is not used\n";
+		}
+	}
+	else if(command=="show"){
+		if(library->books.empty())
+			cout<<"no books are borrowed\n";
+		else
 			for(auto i:library->sort_by_time())
 				cout<<i.show();
-			break;
-		case '9':{
+	}
+	else if(command=="save"){
+		if(!saved){
 			if(name.empty()){
-				cout<<"enter file name:";
+				cout<<"enter file name: ";
 				do
-					getline(cin,name);
+					getline(name);
 				while(name.empty());
 			}
 			ofstream fout(name);
 			if(fout.is_open()){
 				library->save(fout);
-				return false;
+				saved=true;
 			}
 			else
-				cerr<<"error:the file cannot be writed-maybe it is read only?\n";
-			break;
+				cerr<<"error: the file cannot be written - maybe it is read-only?\n";
 		}
-		case '0':
-			cout<<"Are you sure that you do not need to save?[y/n] :";
-			loop:
-			cin>>c;
-			switch(c){
-				case 'y':
-				case 'Y':
-					return false;
-				case 'n':
-				case 'N':
-					break;
-				default:
-					goto loop;
-			}
-			break;
-		default:
-			goto mainloop;
 	}
+	else if(command=="exit"){
+		if(saved)
+			return false;
+		cout<<"Are you sure that you do not need to save? [y/n] :";
+		string judge;
+		loop:
+		getline(judge);
+		if(judge=="y" || judge=="Y")
+			return false;
+		if(judge!="n" && judge!="N")
+			goto loop;
+	}
+	else if(command=="help")
+		cout<<"newid\tcreate a new ID\n"
+			<<"lend\tlend a book\n"
+			<<"return\treturn a book\n"
+			<<"show\tshow the borrowed books in chronological order\n"
+			<<"save\n"
+			<<"exit\n"
+			<<"help\tget help\n"
+			<<"version\tget the version information\n";
+	else if(command=="version")
+		version();
+	else if(!command.empty())
+		cerr<<"error: command not found\n";
 	return true;
 }
 int main(int argc,char **argv){
 	srand(time(nullptr));
 	switch(argc){
 		case 0:
-			cerr<<"error:How do you do it?You make argc==0!\n";
+			cerr<<"error: How do you do it? You make argc==0!\n";
 			return 256;
 		case 1:
-			cerr<<"warning:a new file\n";
+			cerr<<"warning: a new file\n\n";
 			library=new Library;
 			break;
 		case 2:{
 			name=argv[1];
+			if(argv[1][0]=='-'){
+				if(argv[1][1]=='-'){
+					if(name=="--help")
+						cout<<"usage: [./]lib4console [-h | --help] [-v | --version] <file>\n";
+					else if(name=="--version")
+						version();
+					else{
+						cerr<<"error: invalid option\n";
+						return 256;
+					}
+				}
+				else{
+					if(name=="-h")
+						cout<<"usage: [./]lib4console [-h | --help] [-v | --version] <file>\n";
+					else if(name=="-v")
+						version();
+					else{
+						cerr<<"error: invalid option\n";
+						return 256;
+					}
+				}
+				return 0;
+			}
 			ifstream fin(argv[1]);
 			if(!fin.is_open()){
-				cerr<<"error:"<<argv[1]<<":cannot find the file\n";
+				cerr<<"error: "<<argv[1]<<": cannot find the file\n";
 				return 256;
 			}
 			library=new Library(fin);
 			break;
 		}
 		default:
-			cerr<<"error:too many args\n";
+			cerr<<"error: too many args\n";
 			return 256;
 	}
+	version();
+	cout<<"please enter \"help\" for help or \"version\" for version information\n";
 	while(main_menu());
 	delete library;
 	return 0;
